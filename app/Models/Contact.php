d609219bcd864fde4d2f0f7fb618ee015a1212a3
Smolkin Sergiy <37virs85@gmail.com>
2023-05-09 21:50:45 +0200

    wip

--- app/Models/Contact.php
+++ app/Models/Contact.php
@@ -25,4 +25,23 @@ class Contact extends Model
         return $this->belongsTo(Client::class);
     }
 
+    public function scopeFilterFields($query, array $filters)
+    {
+        $query->when($filters['search'] ?? null, function ($query, $search) {
+            $query->where(function ($query) use ($search) {
+                $query->where('first_name', 'like', '%'. $search.'%')
+                    ->orWhere('last_name', 'like', '%'. $search.'%')
+                    ->orWhere('email', 'like', '%'. $search.'%')
+                    ->orWhereHas('organization', function ($query) use ($search) {
+                        $query->where('name', 'like', '%'. $search.'%');
+                    });
+            });
+        })->when($filters['trashed'] ?? null, function ($query, $trashed) {
+            if ($trashed === 'with') {
+                $query->withTrashed();
+            } elseif ($trashed === 'only') {
+                $query->onlyTrashed();
+            }
+        });
+    }
 }
